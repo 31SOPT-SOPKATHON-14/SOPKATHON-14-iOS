1a1960e35b615b4ddce1247a52fd633b457d857d
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-11-19 19:00:42 +0900

    [Chore] 네트워크 초기세팅

---
+++ SOPKATHON/SOPKATHON/Network/Base/Enviroment.swift
@@ -0,0 +1,15 @@
+//
+//  Enviroment.swift
+//  SOPKATHON
+//
+//  Created by 강윤서 on 2022/11/19.
+//
+
+import Foundation
+
+// MARK: - Environment
+
+struct Environment {
+//    static let baseURL = (Bundle.main.infoDictionary?["BASE_URL"] as! String).replacingOccurrences(of: " ", with: "")
+//    static let baseURL = "http://3.34.53.11:8080"
+}
---
+++ SOPKATHON/SOPKATHON/Network/Base/URLConstant.swift
@@ -0,0 +1,12 @@
+//
+//  URLConstant.swift
+//  SOPKATHON
+//
+//  Created by 강윤서 on 2022/11/19.
+//
+
+import Foundation
+
+struct URLConstant {
+    static let urlName = ""
+}
