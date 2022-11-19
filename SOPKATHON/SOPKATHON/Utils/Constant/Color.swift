0a2f43f71d087c1feb15ddffaa31741501b2dc3f
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-11-20 00:01:19 +0900

    [Chore] 초기세팅 변경

--- SOPKATHON/SOPKATHON/Utils/Constant/Color.swift
+++ SOPKATHON/SOPKATHON/Utils/Constant/Color.swift
@@ -5,8 +5,16 @@
 //  Created by 강윤서 on 2022/11/18.
 //
 
-import Foundation
+import UIKit
 
-struct Color {
-    
+extension UIColor {
+    convenience init(r: Int, g:Int, b:Int){
+        self.init(red: CGFloat(r)/255, green: CGFloat(g)/255, blue: CGFloat(b)/255, alpha: 1)
+    }
+}
+
+extension UIColor{
+//    class var camiMint: UIColor{
+//        return UIColor(r: 1, g: 1, b: 1)
+//    }
 }
